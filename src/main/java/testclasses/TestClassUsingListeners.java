5a47d7764ffa2ee366a7eba74198fcc5a0e92da3
Amit Chatterjee <chatterjeeamit41@gmail.com>
2022-04-26 12:20:46 +0530

    first commit

--- src/main/java/testclasses/TestClassUsingListeners.java
+++ src/main/java/testclasses/TestClassUsingListeners.java
@@ -3,6 +3,7 @@ package testclasses;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -21,180 +22,42 @@ import org.testng.annotations.Test;
 import base.TestBase;
 
 public class TestClassUsingListeners {
-public WebDriver driver;
-	
-	
+	public WebDriver driver;
+
 	@BeforeMethod
 	public void setupBrwoser() throws InterruptedException {
-		driver= TestBase.getDriver();
+		driver = TestBase.getDriver();
 		Thread.sleep(3000);
 	}
 
-	@Test(priority=0, enabled= true)
-	private void tc0() throws IOException, InterruptedException {
+	@Test(priority = 0, enabled = true)
+	private void tc1() throws IOException, InterruptedException {
 
-		try {
-			System.out.println("Starting tc0 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[1]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
+		System.out.println("Starting tc1 TC");
+		Thread.sleep(500);
+		driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']")).click();
+		Thread.sleep(500);
+
+		List<WebElement> listofRadioButton = driver.findElements(By.xpath("//*[@class='a-icon a-icon-radio']"));
+		int count = listofRadioButton.size();
+		System.out.println(count);
+
+		for (int i = 0; i < count; i++) {
+			listofRadioButton.get(i).click();
+			File f = new File(System.getProperty("user.dir") + "//data//Book1.xlsx");
 			FileInputStream fi = new FileInputStream(f);
 			Workbook wb = new XSSFWorkbook(fi);
 			Sheet sh = wb.getSheet("Sheet1");
 			Row r = sh.getRow(0);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
+			Cell c = r.getCell(i);
+			Thread.sleep(500);
 			System.out.println(c);
-		} catch (Exception e) {
-
 		}
-
 	}
 
-	@AfterMethod
-	public void TearDown() {
-		driver.close();
-	}
-	
-	
-	
-	
-	@Test(priority=1,enabled= true)
-	private void tc1() throws IOException, InterruptedException {
-
-		try {
-			System.out.println("Starting tc1 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[2]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
-			FileInputStream fi = new FileInputStream(f);
-			Workbook wb = new XSSFWorkbook(fi);
-			Sheet sh = wb.getSheet("Sheet1");
-			Row r = sh.getRow(1);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
-			System.out.println(c);
-		} catch (Exception e) {
-
-		}
-
-	}
-
-	@Test(priority=2,enabled= true)
-	private void tc2() throws IOException, InterruptedException {
-		try {
-			System.out.println("Starting tc2 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[3]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
-			FileInputStream fi = new FileInputStream(f);
-			Workbook wb = new XSSFWorkbook(fi);
-			Sheet sh = wb.getSheet("Sheet1");
-			Row r = sh.getRow(2);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
-			System.out.println(c);
-		} catch (Exception e) {
-
-		}
-
-	}
-
-	@Test(priority=3,enabled= true)
-	private void tc3() throws IOException, InterruptedException {
-
-		try {
-			System.out.println("Starting tc3 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[4]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
-			FileInputStream fi = new FileInputStream(f);
-			Workbook wb = new XSSFWorkbook(fi);
-			Sheet sh = wb.getSheet("Sheet1");
-			Row r = sh.getRow(3);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
-			System.out.println(c);
-		} catch (Exception e) {
-
-		}
-
-	}
-
-	@Test(priority=4,enabled= true)
-	private void tc4() throws IOException, InterruptedException {
-
-		try {
-			System.out.println("Starting tc4 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[5]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
-			FileInputStream fi = new FileInputStream(f);
-			Workbook wb = new XSSFWorkbook(fi);
-			Sheet sh = wb.getSheet("Sheet1");
-			Row r = sh.getRow(4);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
-			System.out.println(c);
-		} catch (Exception e) {
-
-		}
-
-	}
-
-	@Test(priority=5,enabled= true)
-	private void tc5() throws IOException, InterruptedException {
-
-		try {
-			System.out.println("Starting tc5 TC");
-			WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-			Actions a = new Actions(driver);
-			a.moveToElement(logo).perform();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("(//i[@class='icp-radio'])[6]")).click();
-			File f = new File(System.getProperty("user.dir")+ "//data//Book1.xlsx");
-			FileInputStream fi = new FileInputStream(f);
-			Workbook wb = new XSSFWorkbook(fi);
-			Sheet sh = wb.getSheet("Sheet1");
-			Row r = sh.getRow(5);
-			Cell c = r.getCell(0);
-			Thread.sleep(3000);
-			System.out.println(c);
-		} catch (Exception e) {
-
-		}
-
-	}
-
-	@Test(priority=6,enabled= true)
-	private void tc6() throws InterruptedException {
-		System.out.println("Starting tc6 TC");
-		WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
-		Actions a = new Actions(driver);
-		a.moveToElement(logo).perform();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("(//i[@class='icp-radio'])[7]")).click();
-		Thread.sleep(3000);
-	}
-
-	@Test(priority=7,enabled= true)
-	private void tc7() throws InterruptedException {
-		System.out.println("Starting tc7 TC");
+	@Test(priority = 1, enabled = true)
+	private void tc2() throws InterruptedException {
+		System.out.println("Starting tc2 TC");
 		WebElement logo = driver.findElement(By.xpath("//span[@class='icp-nav-flag icp-nav-flag-in']"));
 		Actions a = new Actions(driver);
 		a.moveToElement(logo).perform();
@@ -204,14 +67,23 @@ public WebDriver driver;
 
 	}
 
-	
-	  @Test(priority=8,enabled= true) private void tc8() throws
-	  InterruptedException { System.out.println("Starting tc8 TC"); Assert.fail();
-	  }
-	  
-	  @Test(priority=9,enabled= true) private void tc9() throws
-	  InterruptedException { System.out.println("Starting tc9 TC");
-	  Thread.sleep(3000); Assert.fail(); }
-	 
-}
+	@Test(priority = 2, enabled = true)
+	private void tc3() throws InterruptedException {
+		System.out.println("Starting tc3 TC");
+		Assert.fail();
+		Thread.sleep(3000);
+	}
 
+	@Test(priority = 3, enabled = true)
+	private void tc4() throws InterruptedException {
+		System.out.println("Starting tc4 TC");
+		Assert.fail();
+		Thread.sleep(3000);
+	}
+
+	@AfterMethod
+	public void TearDown() {
+		driver.close();
+	}
+
+}
